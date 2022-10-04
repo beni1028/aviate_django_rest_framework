a59cc7f74c2f440f29d62eb414bf9e424bb07d76
beni1028 <beni1028@gmail.com>
2022-10-04 19:28:04 +0530

    Chnages

--- api/management/commands/create_groups.py
+++ api/management/commands/create_groups.py
@@ -1,7 +1,7 @@
 from django.db import transaction
 from django.core.management.base import BaseCommand
 
-from django.contrib.auth.models import Group, User
+from django.contrib.auth.models import Group
 
 
 class Command(BaseCommand):
